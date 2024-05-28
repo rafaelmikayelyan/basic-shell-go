03ba14d1fbc12f28628952603e75f381605798db
Rafael Mikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2024-05-27 18:30:49 -0700

    feat: add echo builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,11 +13,17 @@ func main() {
 
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 		input = input[:len(input) - 1]
+		inputs := strings.Fields(input)
+
+
 		
-		if input == "exit 0" {
-			os.Exit(0)
-		} else {
-			fmt.Fprint(os.Stdout, input + ": command not found\n")
+		switch {
+			case input == "exit 0":
+				os.Exit(0)
+			case inputs[0] == "echo":
+				fmt.Fprint(os.Stdout, input[5:] + "\n")
+			default:
+				fmt.Fprint(os.Stdout, input + ": command not found\n")
 		}
 	}
 }

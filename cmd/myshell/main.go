3736bbf053f613a4e6825c9afbd0ef8f95a8b2fb
Rafael Mikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2024-05-27 17:50:45 -0700

    feat: add REPL

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,9 +7,11 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	
-	fmt.Fprint(os.Stdout, input[:len(input)-1] + ": command not found\n")
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		
+		fmt.Fprint(os.Stdout, input[:len(input) - 1] + ": command not found\n")
+	}
 }

7a4daf443a0ca5053613ac94f685963025586f7d
Rafael Mikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2024-05-27 19:29:20 -0700

    feat: add type for builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,6 +8,8 @@ import (
 )
 
 func main() {
+	builtins := [3]string{"exit", "echo", "type"}
+
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
@@ -15,13 +17,28 @@ func main() {
 		input = input[:len(input) - 1]
 		inputs := strings.Fields(input)
 
-
-		
 		switch {
 			case input == "exit 0":
 				os.Exit(0)
 			case inputs[0] == "echo":
 				fmt.Fprint(os.Stdout, input[5:] + "\n")
+			case inputs[0] == "type":
+				if len(inputs) == 2 {
+					isFound := false
+					for _, builtin := range builtins {
+						if builtin == inputs[1] {
+							isFound = true
+							break
+						}
+					}
+					if isFound {
+							fmt.Fprint(os.Stdout, inputs[1] + " is a shell builtin\n")
+					} else {
+							fmt.Fprint(os.Stdout, inputs[1] + " not found\n")
+					}
+				} else {
+					fmt.Fprint(os.Stdout, "usage: type [command]\n")
+				}
 			default:
 				fmt.Fprint(os.Stdout, input + ": command not found\n")
 		}
